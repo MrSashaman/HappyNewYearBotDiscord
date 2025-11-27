214b028cf9e6ecff210537cdd2f49cc070ca1920
mrSashaman_ <psasha2811@gmail.com>
2025-11-27 16:35:01 +1200

    0.2

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ async def ping(ctx):
 
 @bot.command()
 async def checkrole(ctx):
-    role_id = 1443444045966737428
+    role_id = "айди роли"
 
     role = ctx.guild.get_role(role_id)
 
     if role in ctx.author.roles:
-        await ctx.send("Окей")
+        await ctx.send(F"Окей Я рад что у вас есть роль {role}")
         await ctx.send("Вот картинка:", file=discord.File("avarter.png"))
     else:
         await ctx.send("У вас нет этой роли.")
 
-bot.run('Введи свой токен')
+bot.run('токен')
