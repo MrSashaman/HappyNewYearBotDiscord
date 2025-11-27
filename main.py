3a27e551c6a032b427694ab22f0a78dcbddabc9d
mrSashaman_ <psasha2811@gmail.com>
2025-11-27 16:29:51 +1200

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+@bot.command()
+async def checkrole(ctx):
+    role_id = 1443444045966737428
+
+    role = ctx.guild.get_role(role_id)
+
+    if role in ctx.author.roles:
+        await ctx.send("Окей")
+        await ctx.send("Вот картинка:", file=discord.File("avarter.png"))
+    else:
+        await ctx.send("У вас нет этой роли.")
+
+bot.run('Введи свой токен')
