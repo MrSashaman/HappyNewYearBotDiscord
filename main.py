271fe448d9efd70b0878d0dc2c92f889354e88f4
mrSashaman_ <psasha2811@gmail.com>
2025-11-28 19:02:41 +1200

    0.4

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import requests
 import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
+API_KEY = 'апи'
 
 @bot.command()
 async def ping(ctx):
@@ -11,14 +13,30 @@ async def ping(ctx):
 
 
 @bot.command()
-async def whtweather(ctx):
+async def weather(ctx):
     role_id = 1443444045966737428
-
     role = ctx.guild.get_role(role_id)
 
     if role in ctx.author.roles:
-        await ctx.send(F"Окей Я рад что у вас есть роль {role}")
-        await ctx.send("Вот актуальная погода:  В РАЗРАБОТКЕ")
+        city = "Петропавловск-Камчатский"
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&lang=ru'
+
+        response = requests.get(url)
+        data = response.json()
+
+        if data.get("cod") == 200:
+            weather_description = data["weather"][0]["description"]
+            temp = data["main"]["temp"]
+            humidity = data["main"]["humidity"]
+            wind_speed = data["wind"]["speed"]
+
+            await ctx.send(f"Вот актуальная погода в {city}:")
+            await ctx.send(f"Температура: {temp}°C\n"
+                           f"Описание: {weather_description}\n"
+                           f"Влажность: {humidity}%\n"
+                           f"Скорость ветра: {wind_speed} м/с")
+        else:
+            await ctx.send("Не удалось получить данные о погоде.")
     else:
         await ctx.send("У вас нет этой роли.")
 
@@ -34,4 +52,4 @@ async def checkrole(ctx):
     else:
         await ctx.send("У вас нет этой роли.")
 
-bot.run('token')
+bot.run('токен')
